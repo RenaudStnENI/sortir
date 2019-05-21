f560d7a5170475958df08e88a8b28ebb7098b607
Virgil <virgil.cauvin2019@campus-eni.fr>
2019-05-21 12:16:38 +0200

    Creation sortieType + modif eventcontroller, add.twig, list.twig

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Sortie;
+use App\Form\SortieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends Controller
@@ -20,5 +22,23 @@ class EventController extends Controller
         //$choses=$ideaRepo->findAll();
         $events = $sortiesRepo->findBy([], ["dateLimiteInscription" => "DESC"], 30);
         return $this->render('event/list.html.twig', ["events"=>$events,"title"=>$title]);
+    }
+
+    /**
+     * @Route("/add", name="add")
+     */
+    public function add(EntityManagerInterface $em, Request $request)
+    {
+        $sortie = new Sortie();
+        $sortieForm = $this->createForm(SortieType::class,$sortie);
+        $sortieForm->handleRequest($request);
+        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
+            $em->persist();
+            $em->flush();
+            $this->addFlash('success','La sortie est bien ajoutée !');
+            return $this->redirectToRoute("list");
+        }
+        $title="add";
+        return $this->render('event/add.html.twig', ["sortieForm"=>$sortieForm->createView(),"title"=>$title]);
     }
 }
---
+++ src/Form/SortieType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Sortie;
+use App\Entity\Ville;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SortieType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('nom', TextType::class,['label'=>'Nom de la sortie : '])
+            ->add('dateHeureDebut',DateType::class,['label'=>'Date et heure de sortie : ','widget'=>'single_text','html5'=>false,'attr'=>['class'=>'js-datepicker']])
+            ->add('duree')
+            ->add('dateLimiteInscription')
+            ->add('nbInscriptionsMax')
+            ->add('infosSortie')
+            ->add('lieu')
+            ->add('ville',EntityType::class,['class'=>Ville::class,'mapped'=>false])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Sortie::class,
+        ]);
+    }
+}
