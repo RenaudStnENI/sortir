72b67a57bbea5686634faa352de38003b403170a
clementine.prigent2019 <clementine.prigent2019@campus-eni.fr>
2019-05-21 10:37:34 +0200

    création de toutes les relations entre

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,11 +2,50 @@
 
 namespace App\Controller;
 
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\BrowserKit\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UserController extends Controller
 {
+    /**
+     * @Route("/monProfil", name="monProfil")
+     */
+    public function monProfil (Request $request, EntityManagerInterface $em)
+    {
+        $userRepo = $this->getDoctrine()->getRepository(User::class);
+        $user = $userRepo->find(1);
+
+        $registerForm=$this->createForm(RegisterType::class,$user);
+        $registerForm->handleRequest($request);
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     /**
      * @Route("/user", name="user")
      */
