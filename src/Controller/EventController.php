f2711980d0679b54a06661b0b342b2cea3a3ad19
Virgil <virgil.cauvin2019@campus-eni.fr>
2019-05-21 16:54:44 +0200

    modif event controller + sortietype + add.twig + sortie.php + css

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -2,17 +2,19 @@
 
 namespace App\Controller;
 
+use App\Entity\Etat;
 use App\Entity\Sortie;
 use App\Form\SortieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends Controller
 {
     /**
-     * @Route("/", name="list")
+     * @Route("/sortir", name="list")
      */
     public function list(EntityManagerInterface $em)
     {
@@ -25,18 +27,26 @@ class EventController extends Controller
     }
 
     /**
-     * @Route("/add", name="add")
+     * @Route("/sortir/add", name="add")
      */
     public function add(EntityManagerInterface $em, Request $request)
     {
         $sortie = new Sortie();
+        $sortie->setDateHeureDebut(new \DateTime());
+        $etat = $this->getDoctrine()->getManager()->getReference(Etat::class, 1);
+        $sortie->setEtat($etat);
         $sortieForm = $this->createForm(SortieType::class,$sortie);
         $sortieForm->handleRequest($request);
-        if($sortieForm->isSubmitted() && $sortieForm->isValid()){
-            $em->persist();
-            $em->flush();
-            $this->addFlash('success','La sortie est bien ajoutée !');
-            return $this->redirectToRoute("list");
+
+        if($sortie->getDateLimiteInscription() < $sortie->getDateHeureDebut()) {
+            if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
+                $em->persist($sortie);
+                $em->flush();
+                $this->addFlash('success', 'La sortie est bien ajoutée !');
+                return $this->redirectToRoute("list");
+            }
+        }else{
+            $sortieForm->get('dateLimiteInscription')->addError(new FormError('La date de limite d\'inscription doit etre inferieur à la date de la sortie !'));
         }
         $title="add";
         return $this->render('event/add.html.twig', ["sortieForm"=>$sortieForm->createView(),"title"=>$title]);
