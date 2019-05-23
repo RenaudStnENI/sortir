302ddfeb6c51f5681850800f11f6aaca2315db54
Virgil <virgil.cauvin2019@campus-eni.fr>
2019-05-23 16:00:55 +0200

    modif eventController + sortie.php + sortieType + add.twig

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -10,8 +10,8 @@ use App\Form\SortieType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\FormError;
-use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class EventController extends Controller
@@ -42,23 +42,6 @@ class EventController extends Controller
         $sortieForm = $this->createForm(SortieType::class,$sortie);
         $sortieForm->handleRequest($request);
 
-        $villeRepo = $this->getDoctrine()->getRepository(Ville::class);
-        $ville = $villeRepo->findAll();
-        $lieuRepo = $this->getDoctrine()->getRepository(Lieu::class);
-        $lieu = $lieuRepo->findAll();
-
-        //equivalent header
-        $response = new JsonResponse();
-        $response->setContent(json_encode([
-            'data' => $ville,
-        ]));
-        $response->headers->set('Content-Type', 'application/json');
-
-        //if($ville->getCp()){
-
-        //}
-
-
         if($sortie->getDateLimiteInscription() < $sortie->getDateHeureDebut()) {
             if ($sortieForm->isSubmitted() && $sortieForm->isValid()) {
                 if ($sortieForm->get('publier')->isclicked()) {
@@ -66,7 +49,7 @@ class EventController extends Controller
                     $sortie->setEtat($etat);
                     $em->persist($sortie);
                     $em->flush();
-                    $this->addFlash('success', 'La sortie est bien ajoutée !');
+                    $this->addFlash('success', 'La sortie est bien publié !');
                     return $this->redirectToRoute("list");
                 }elseif ($sortieForm->get('enregistrer')->isClicked()){
                     $etat = $this->getDoctrine()->getManager()->getReference(Etat::class, 1);
@@ -82,6 +65,43 @@ class EventController extends Controller
             $sortieForm->get('dateLimiteInscription')->addError(new FormError('La date de limite d\'inscription doit etre inferieur à la date de la sortie !'));
         }
         $title="add";
-        return $this->render('event/add.html.twig', ["sortieForm"=>$sortieForm->createView(),"title"=>$title, "lieu"=>$lieu, "ville"=>$ville]);
+        return $this->render('event/add.html.twig', ["sortieForm"=>$sortieForm->createView(),"title"=>$title]);
+    }
+
+    /**
+     * @Route("/sortir/add/requeteAjax", name="requeteAjax")
+     */
+    public function requeteAjax(Request $request, EntityManagerInterface $em){
+        $select = $request->request->get('choix');
+        $lieux = $em->getRepository(Lieu::class)->findBy(array('ville'=>$select));
+
+        $lieuTab = [];
+        foreach ($lieux as $lieu){
+            $lieuTab[$lieu->getId()] = $lieu->getNom();
+        }
+
+        $response = new Response(json_encode($lieuTab));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
+    }
+
+    /**
+     * @Route("/sortir/add/requeteLieu", name="requeteLieu")
+     */
+    public function requeteLieu(Request $request, EntityManagerInterface $em){
+        $infoLieu = $request->request->get('detailLieu');
+        $detail = $em->getRepository(Lieu::class)->find($infoLieu);
+
+        $lieu = [
+          'rue' => $detail->getRue(),
+           'latitude' => $detail->getLatitude(),
+            'longitude' => $detail->getLongitude()
+        ];
+
+        $response = new Response(json_encode($lieu));
+        $response->headers->set('Content-Type', 'application/json');
+
+        return $response;
     }
 }
--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -2,13 +2,10 @@
 
 namespace App\Form;
 
-use App\Entity\Etat;
-use App\Entity\Site;
 use App\Entity\Sortie;
 use App\Entity\Ville;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
@@ -26,13 +23,14 @@ class SortieType extends AbstractType
         $builder
             ->add('nom', TextType::class,['label'=>'Nom de la sortie : '])
             ->add('dateHeureDebut',DateTimeType::class,['label'=>'Date et heure de sortie : ','widget'=>'single_text','html5'=>true/*,'attr'=>['class'=>'js-datepicker']*/])
-            ->add('duree', TimeType::class,['label'=>'Durée : ','widget'=>'single_text', 'html5'=>true])
+            ->add('duree', IntegerType::class,['label'=>'Durée : '])
             ->add('dateLimiteInscription', DateType::class,['label'=>'Date limite d\'inscription : ','widget'=>'single_text','html5'=>true/*,'attr'=>['class'=>'js-datepicker']*/])
             ->add('nbInscriptionsMax', IntegerType::class,['label'=>'Nombre de participant maximum : '])
             ->add('infosSortie', TextareaType::class,['label'=>'Description et infos : '])
             ->add('lieu',null,['label'=>'Lieu : ', 'choice_label'=>'nom'])
             ->add('ville',EntityType::class,['class'=>Ville::class,'mapped'=>false,'label'=>'Ville : ','choice_label'=>'nom'])
             ->add('publier', SubmitType::class,['label'=>'Publier la sortie'])
+            ->add('enregistrer', SubmitType::class,['label'=>'Enregitrer'])
         ;
     }
 
